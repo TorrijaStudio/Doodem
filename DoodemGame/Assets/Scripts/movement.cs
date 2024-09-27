9c296b465f3e9c21a26b95e97e3a50dc5d294dc8
leninn23 <leninanderson23@gmail.com>
2024-09-27 13:48:48 +0200

    primer uso de navmesh
    movimiento y ataque

---
+++ DoodemGame/Assets/Scripts/movement.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
+
+public class movement : MonoBehaviour
+{
+    private NavMeshAgent agente;
+    private float timeLastHit;
+    private Transform currentObjective;
+
+    public Transform objetive;
+    public float health;
+    public float damage;
+    public float attackDistance;
+    public float attackSpeed;
+
+    void Start()
+    {
+        currentObjective = objetive;
+        agente = GetComponent<NavMeshAgent>();
+        agente.SetDestination(objetive.position);
+    }
+
+    void Update()
+    {
+        
+        if (currentObjective)
+        {
+            if (Vector3.Distance(agente.transform.position, currentObjective.position) > attackDistance)
+            {
+                if (agente.destination != currentObjective.position)
+                {
+                    //agente.SetDestination(currentObjective.position);
+                }
+            }
+        
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(agente.transform.position, attackDistance, LayerMask.GetMask("Enemy"));
+        if (hitColliders.Length==0) return;
+        foreach (var c in hitColliders)
+        {
+            if (c.gameObject != gameObject)
+            {
+                currentObjective = c.transform;
+                agente.SetDestination(currentObjective.position);
+                break;
+            }
+        }
+        
+        //Debug.Log(hitColliders[0].name);
+        if (currentObjective)
+        {
+            if (Time.time - timeLastHit >= 1f / attackSpeed)
+            {
+                float aux = 0;
+                if (currentObjective.TryGetComponent(out movement m))
+                {
+                    aux = m.Attacked(damage);
+                }
+                
+                if (aux < 0)
+                {
+                    Debug.Log(gameObject.name+"  "+objetive.position);
+                    currentObjective = objetive;
+                    agente.SetDestination(objetive.position);
+                }
+                timeLastHit = Time.time;
+            }
+            
+        }
+    }
+
+    public float Attacked(float enemyDamage)
+    {
+        Debug.Log(gameObject.name +"-- "+health);
+        health -= enemyDamage;
+        if (health < 0)
+        {
+            Destroy(gameObject);
+        }
+
+        return health;
+    }
+}
